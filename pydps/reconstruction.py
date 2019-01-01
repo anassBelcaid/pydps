5558a6f2161f2adc5845d33bf5c4831699cc0655
anass <anass.belcaid@gmail.com>
2019-01-01 17:35:25 +0000

    Reconstruction by geman matrix

--- pydps/reconstruction.py
+++ pydps/reconstruction.py
@@ -6,6 +6,7 @@ contour
 
 import numpy as np
 import torch
+from pydps.linalg import gemanPriorMatrix
 import matplotlib.pyplot as plt
 
 
@@ -53,17 +54,68 @@ def reconstruct_mean_from_edge1d(signal,edge):
 
     return recovered
 
+def reconstruct_median_from_edge1d(signal,edge):
+    """Function to replace each block by its mean given the contours
+
+    :signal: torch tensor 1D (dim N)  noised version of the signal
+    :edge: torch tensor 1D (dim N-1) edges for the signal
+    :returns: recoverd signal by replacing each block by it's mean
+    """
+
+    #getting the blocks
+    blocks = blocks_from_edge1d(edge)
 
 
+    #recovered signal
+    recovered = torch.zeros_like(signal)
+
+    for (l,r) in blocks:
+        recovered.numpy()[l:r] = np.median(signal[l:r].numpy())
+
+    return recovered
+
+
+
+def _reconstruct_geman(signal,lam):
+    """ Reconstruct the signal by considering the continuous geman model
+    :signal: torch tensor 
+    :lam : Smoothness power
+    :returns: solution to the the (Ax= signal) where A is the continuous matrix
+    """
+
+    A =gemanPriorMatrix(lam,len(signal),factorized=True)
+    
+
+    return torch.potrs(signal,A.float(),upper=False)
+    
+
+def reconstruct_geman_from_edge1d(signal,edge,lam):
+    """Function to replace each block by its mean given the contours
+
+    :signal: torch tensor 1D (dim N)  noised version of the signal
+    :edge: torch tensor 1D (dim N-1) edges for the signal
+    :returns: recoverd signal by replacing each block by it's mean
+    """
+
+    #getting the blocks
+    blocks = blocks_from_edge1d(edge)
+
+
+    #recovered signal
+    recovered = torch.zeros_like(signal)
+    for (l,r) in blocks:
+        recovered[l:r] = _reconstruct_geman(signal[l:r],lam).squeeze()
+
+    return recovered
 if __name__ == "__main__":
 
-    
+    lam =3 
     signal  =   torch.randn(12)
     edge   = torch.tensor([0,0,0,1,0,0,0,1,0,0,0])
 
-    recoverd = reconstruct_mean_from_edge1d(signal,edge)
+    recovered  = reconstruct_geman_from_edge1d(signal,edge,lam) 
     plt.plot(signal.numpy())
-    plt.stem(recoverd.numpy())
+    plt.stem(recovered.numpy())
     plt.show()
 
 
--- scripts/05_top_hat_data_experience.py
+++ scripts/05_top_hat_data_experience.py
@@ -8,6 +8,8 @@ import torch
 import matplotlib.pyplot as plt
 from pydps.filter1d import DpsFilter1D
 from pydps.reconstruction import reconstruct_mean_from_edge1d
+from pydps.reconstruction import reconstruct_median_from_edge1d
+from pydps.reconstruction import reconstruct_geman_from_edge1d
 
 
 if __name__ == "__main__":
@@ -16,14 +18,14 @@ if __name__ == "__main__":
     data, target = load_top_hat(128,0.5)
 
     #filtering 
-    filt = DpsFilter1D(40, 0.8,8)
+    filt = DpsFilter1D(20, 0.8,8)
 
     line,grad,(left,right) = filt(data)
 
-    recoverd = reconstruct_mean_from_edge1d(data,line)
+    recoverd = reconstruct_geman_from_edge1d(data,line,40)
     
     plt.plot(target.numpy())
     plt.plot(data.numpy(),alpha=0.6,lw=0.5)
-    plt.plot(recoverd.numpy())
+    plt.plot(recoverd.numpy(),label='rec')
     plt.legend()
     plt.show()
