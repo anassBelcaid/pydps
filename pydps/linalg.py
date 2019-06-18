35872ca21db6fd454ecc0c2ac832f07b3f77647f
Anass Belcaid <anass.belcaid@gmail.com>
2019-06-18 15:29:27 +0200

    Geman matrix from  gradient

--- pydps/linalg.py
+++ pydps/linalg.py
@@ -4,6 +4,7 @@ Function and helper for all the linear algebra operations
 import numpy as np
 import torch
 import scipy.linalg as linAlg
+from scipy.sparse import dok_matrix
 
 
 #{{{ Helper function to move from condensed to full
@@ -42,6 +43,45 @@ def gemanPriorMatrix(lam,size,factorized=True):
         if(factorized):
             Ab = torch.cholesky(Ab,upper=False)
         return Ab
+#}}}
+#{{{ Geman matrix for an image
+def gemanPriorMatrix2d_fromGrad(lam, H, V):
+    """
+    genreate full factorized version of the geman 
+    matrix in 2d given the Horizontal and vertical gradient
+
+    :param lam: regularaization factor
+    :param H  : Horizontal boolean process (n, n-1) 
+    :param V  : Vertical boolean process (n-1, n)
+
+    :return M : full matrix size (n**2, n**2)
+    """
+
+    #common size
+    n = H.shape[0]
+    n_sq = n**2
+
+
+    #initiate with the identity matrix
+    M = dok_matrix((n_sq, n_sq), dtype=np.float32)
+
+    #squared lambda
+    lam2 = lam**2
+
+    #filling the horizontal graph
+    for i in range(n):
+        for j in range(n-1):
+            if H[i][j]== 1:
+                #index in the matrix
+                index_l = np.unravel_index((i,j), (n,n))  #index left node
+                index_r = np.unravel_index((i,i+1), (n,n)) #index right node
+                M[index_l, index_l]   += lam2
+                M[index_r, index_r]   += lam2
+                M[index_l, index_r]  = lam2 
+                M[index_r, index_l] = lam2
+                
+
+    return  M.tocsr()
 #}}}
 #{{{ Illustrate potrs
 def illustrate_potrs():
